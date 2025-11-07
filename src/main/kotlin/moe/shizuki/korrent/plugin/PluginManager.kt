43aca2ed3706c282cb7d25b3a2c55445768d7e4d
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2025-11-07 14:26:46 +0800

    feat: plugin support

--- src/main/kotlin/moe/shizuki/korrent/plugin/PluginManager.kt
+++ src/main/kotlin/moe/shizuki/korrent/plugin/PluginManager.kt
@@ -1,6 +1,7 @@
 package moe.shizuki.korrent.plugin
 
 import org.pf4j.DefaultPluginManager
+import org.pf4j.PluginState
 import org.springframework.stereotype.Component
 import java.nio.file.Path
 
@@ -12,23 +13,39 @@ class PluginManager {
         pluginManager.loadPlugins()
     }
 
-    fun loadPlugin(pluginPath: Path) {
-        pluginManager.loadPlugin(pluginPath)
+    fun loadPlugin(pluginPath: Path): String? {
+        return pluginManager.loadPlugin(pluginPath)
     }
 
     fun unloadPlugins() {
         pluginManager.unloadPlugins()
     }
 
-    fun unloadPlugin(pluginId: String) {
-        pluginManager.unloadPlugin(pluginId)
+    fun unloadPlugin(pluginId: String): Boolean {
+        return pluginManager.unloadPlugin(pluginId)
     }
 
-    fun enablePlugin(pluginId: String) {
-        pluginManager.enablePlugin(pluginId)
+    fun enablePlugin(pluginId: String): Boolean {
+        return pluginManager.enablePlugin(pluginId)
     }
 
-    fun disablePlugin(pluginId: String) {
-        pluginManager.disablePlugin(pluginId)
+    fun disablePlugin(pluginId: String): Boolean {
+        return pluginManager.disablePlugin(pluginId)
+    }
+
+    fun startPlugins() {
+        pluginManager.startPlugins()
+    }
+
+    fun startPlugin(pluginId: String): PluginState? {
+        return pluginManager.startPlugin(pluginId)
+    }
+
+    fun stopPlugins() {
+        pluginManager.stopPlugins()
+    }
+
+    fun stopPlugin(pluginId: String): PluginState? {
+        return pluginManager.stopPlugin(pluginId)
     }
 }
