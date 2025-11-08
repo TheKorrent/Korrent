3721c6ede9114e264094fe0cfdcbf2ce6dd317a2
Shizuki Natsuki <shizuki.natsuki@outlook.com>
2025-11-08 23:01:52 +0800

    fix: the plugins cannot listen to events (#18)

--- src/main/kotlin/moe/shizuki/korrent/plugin/KorrentPlugin.kt
+++ src/main/kotlin/moe/shizuki/korrent/plugin/KorrentPlugin.kt
@@ -1,5 +1,8 @@
 package moe.shizuki.korrent.plugin
 
+import com.google.common.eventbus.EventBus
 import org.pf4j.ExtensionPoint
 
-interface KorrentPlugin: ExtensionPoint
+interface KorrentPlugin: ExtensionPoint {
+    fun initialize(eventBus: EventBus)
+}
