4fa790a289939af43ba8eea16b1dd827389d6a19
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2025-11-16 13:58:53 +0800

    feat: add korrent config

--- src/main/kotlin/moe/shizuki/korrent/config/BeanConfig.kt
+++ src/main/kotlin/moe/shizuki/korrent/config/BeanConfig.kt
@@ -1,9 +1,11 @@
 package moe.shizuki.korrent.config
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.google.common.eventbus.EventBus
 import org.pf4j.DefaultPluginManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.File
 
 @Configuration
 class BeanConfig {
@@ -15,5 +17,16 @@ class BeanConfig {
     @Bean
     fun pluginManager(): DefaultPluginManager {
         return DefaultPluginManager()
+    }
+
+    @Bean
+    fun korrentConfig(): KorrentConfig {
+        val configFile = File("config/korrent.json")
+
+        if (!configFile.exists()) {
+            jacksonObjectMapper().writerWithDefaultPrettyPrinter().writeValue(configFile, KorrentConfig("Korrent"))
+        }
+
+        return jacksonObjectMapper().readValue(File("config/korrent.json").readText(), KorrentConfig::class.java)
     }
 }
