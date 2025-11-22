4567a44efa644f824c6533124c56f57232c17d5c
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2025-11-22 18:38:35 +0800

    refactor: convert the manager to a constant

--- src/main/kotlin/moe/shizuki/korrent/bittorrent/client/BitTorrentClient.kt
+++ src/main/kotlin/moe/shizuki/korrent/bittorrent/client/BitTorrentClient.kt
@@ -2,13 +2,8 @@ package moe.shizuki.korrent.bittorrent.client
 
 import moe.shizuki.korrent.bittorrent.config.BitTorrentConfig
 import moe.shizuki.korrent.bittorrent.model.BitTorrentClientType
-import moe.shizuki.korrent.plugin.config.PluginConfigManager
-import moe.shizuki.korrent.plugin.data.PluginDataManager
 
 abstract class BitTorrentClient {
     abstract val clientType: BitTorrentClientType
     abstract val clientConfig: BitTorrentConfig
-
-    val pluginConfigManager: PluginConfigManager = PluginConfigManager()
-    val pluginDataManager = PluginDataManager()
 }
--- src/main/kotlin/moe/shizuki/korrent/plugin/data/PluginDataManager.kt
+++
@@ -1,8 +0,0 @@
-package moe.shizuki.korrent.plugin.data
-
-import moe.shizuki.korrent.pluginDataFolder
-import java.io.File
-
-class PluginDataManager {
-    val folder: File = pluginDataFolder
-}
