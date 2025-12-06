a35bd2bd77884edba56d1ee2c26084f52f2c333e
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2025-12-06 20:43:32 +0800

    refactor: remove id and logger

--- src/main/kotlin/moe/shizuki/korrent/plugin/KorrentPlugin.kt
+++ src/main/kotlin/moe/shizuki/korrent/plugin/KorrentPlugin.kt
@@ -1,6 +1,5 @@
 package moe.shizuki.korrent.plugin
 
-import io.github.oshai.kotlinlogging.KLogger
 import moe.shizuki.korrent.plugin.annotation.processor.KorrentConfigProcessor
 import moe.shizuki.korrent.plugin.annotation.processor.KorrentEventProcessor
 import moe.shizuki.korrent.plugin.annotation.processor.KorrentScheduleEventProcessor
@@ -8,28 +7,25 @@ import org.pf4j.Plugin
 import org.pf4j.PluginWrapper
 
 abstract class KorrentPlugin(wrapper: PluginWrapper): Plugin(wrapper) {
-    abstract val logger: KLogger
-    abstract val id: String
-
     override fun start() {
         super.start()
 
-        KorrentEventProcessor.register(id, wrapper.pluginClassLoader)
-        KorrentConfigProcessor.register(id, wrapper.pluginClassLoader)
-        KorrentScheduleEventProcessor.register(id, wrapper.pluginClassLoader)
+        KorrentEventProcessor.register(wrapper.pluginId, wrapper.pluginClassLoader)
+        KorrentConfigProcessor.register(wrapper.pluginId, wrapper.pluginClassLoader)
+        KorrentScheduleEventProcessor.register(wrapper.pluginId, wrapper.pluginClassLoader)
     }
 
     override fun stop() {
         super.stop()
 
-        KorrentEventProcessor.unregister(id)
-        KorrentScheduleEventProcessor.unregister(id)
+        KorrentEventProcessor.unregister(wrapper.pluginId)
+        KorrentScheduleEventProcessor.unregister(wrapper.pluginId)
     }
 
     override fun delete() {
         super.delete()
 
-        KorrentEventProcessor.unregister(id)
-        KorrentScheduleEventProcessor.unregister(id)
+        KorrentEventProcessor.unregister(wrapper.pluginId)
+        KorrentScheduleEventProcessor.unregister(wrapper.pluginId)
     }
 }
