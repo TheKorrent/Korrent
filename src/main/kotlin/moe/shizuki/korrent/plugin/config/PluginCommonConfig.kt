f8c6ea4e43fe63e0ef986e4fbc1a9f8afa84e6c6
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2025-12-07 11:32:54 +0800

    feat: remove schedule for customization

--- src/main/kotlin/moe/shizuki/korrent/plugin/config/PluginCommonConfig.kt
+++ src/main/kotlin/moe/shizuki/korrent/plugin/config/PluginCommonConfig.kt
@@ -1,14 +1,8 @@
 package moe.shizuki.korrent.plugin.config
 
 class PluginCommonConfig(
-    val schedule: Schedule = Schedule(),
     val proxy: Proxy  = Proxy()
 ) {
-    class Schedule(
-        val cron: String = "*/5 * * * * *",
-        val enabled: Boolean = false
-    )
-
     class Proxy(
         val host: String = "127.0.0.1",
         val port: Int = 1080,
