8fa64c32f78b8a603146f281eca44b99618f0488
Shizuki Natsuki <shizuki.natsuki@outlook.com>
2025-12-09 16:07:00 +0800

    feat: add plugin data manager (#90)

--- src/main/kotlin/moe/shizuki/korrent/plugin/KorrentPlugin.kt
+++ src/main/kotlin/moe/shizuki/korrent/plugin/KorrentPlugin.kt
@@ -4,11 +4,13 @@ import moe.shizuki.korrent.plugin.annotation.processor.KorrentConfigProcessor
 import moe.shizuki.korrent.plugin.annotation.processor.KorrentEventProcessor
 import moe.shizuki.korrent.plugin.annotation.processor.KorrentScheduleEventProcessor
 import moe.shizuki.korrent.plugin.config.PluginConfigManager
+import moe.shizuki.korrent.plugin.data.PluginDataManager
 import org.pf4j.Plugin
 import org.pf4j.PluginWrapper
 
 abstract class KorrentPlugin(wrapper: PluginWrapper): Plugin(wrapper) {
     val pluginConfigManager = PluginConfigManager(wrapper.pluginId)
+    val pluginDataManager = PluginDataManager(wrapper.pluginId)
 
     override fun start() {
         super.start()
